00305b1573b73562d07aeaaa234015add7a592cd
devnandito <fhersa@gmail.com>
2022-10-21 12:22:07 -0300

    delete load .env

--- lib/conf.go
+++ lib/conf.go
@@ -2,10 +2,8 @@ package lib
 
 import (
 	"fmt"
-	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -22,11 +20,7 @@ type Config struct {
 
 // GetEnv load env
 func GetEnv() (host, name, user, pwd, port string) {
-	err := godotenv.Load("lib/.env")
-	if err != nil {
-	 	log.Fatal("Error loading .env file")
-	}
-
+	
 	dbHost := os.Getenv("DB_HOST")
 	dbName := os.Getenv("DB_NAME")
 	dbUser := os.Getenv("DB_USER")
